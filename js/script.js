bc533c908a1f2862deefdfe026719d5e659e29bd
Andrei Malets <coolviktori@yandex.by>
2021-10-27 22:09:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const header = document.querySelector('.site-header');
+const spider = document.querySelector('.spider');
+
+function scarySound() {
+    const audio = new Audio();
+    audio.preload = 'auto';
+    audio.src = 'second.mp3';
+    audio.play();
+
+}
+spider.onclick =scarySound;
+
+
+document.querySelector('.halloween-on').onchange =function(){
+    if (this.checked) {
+        header.classList.add('site-header-bg');
+        spider.classList.add('spider-animation');
+    }
+    else {
+        header.classList.remove('site-header-bg');
+        spider.classList.remove('spider-animation');
+
+    }
+}
